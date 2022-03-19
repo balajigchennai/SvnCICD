5cae448e487d0cced9421a822d3b1b26af6e7e6f
BALAJIGCHENNAI <BALAJIGCHENNAI@GMAIL.COM>
2022-03-19 15:03:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Azure Cloud");
+            try
+            {
+                string spname = System.Configuration.ConfigurationManager.AppSettings["StoreProcedure"].ToString();
+                SqlDriver obj = new SqlDriver();
+                DataTable dtInfo = new DataTable();
+                dtInfo = obj.GetData(spname);
+
+                Console.WriteLine("Count:" + dtInfo.Rows.Count);
+                foreach (DataRow dataRow in dtInfo.Rows)
+                {
+                    Console.WriteLine(dataRow[0] +"-" + dataRow[1]);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured while invoking data  from Azure DB: " + ex.InnerException.ToString());
+               
+            }
+
+        }
+    }
+}
---
+++ SqlDriver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class SqlDriver
+    {
+        private string GetConnectionString()
+        {
+            string connstr = System.Configuration.ConfigurationManager.ConnectionStrings["ConnStringDB"].ConnectionString;
+
+            return connstr;
+        }
+
+        public DataTable GetData(string SpName)
+        {
+            DataTable dt = new DataTable();
+            string cs = string.Empty;
+            cs = GetConnectionString();
+            try{
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = SpName;
+                    con.Open();
+
+                    SqlDataAdapter sqlobj = new SqlDataAdapter(cmd);
+                    sqlobj.Fill(dt);
+
+                    con.Close();
+
+                }
+
+                return dt;
+            }
+            catch(Exception ex) {
+                Console.WriteLine("Error occured while getting information from Azure DB: " + ex.InnerException.ToString());
+                return dt;
+            }
+        }
+
+    }
+}
